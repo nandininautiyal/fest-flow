3cbde435f8c6f80e2a72c581c912093f41060c79
Nandini Nautiyal <nautiyalnandini17@gmail.com>
2026-06-16 22:36:57 +0530

    fix navbar import case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import BrowseEvents from './pages/BrowseEvents'
